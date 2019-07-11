437023610fa94608aa49420b8a47f766aa15b428
Sinahamm <48954716+Sinahamm@users.noreply.github.com>
2019-07-11 11:30:26 +0200

    Update password_forgot.php
    Passwort vergessen mit CSS fertig.

--- password_forgot.php
+++ password_forgot.php
@@ -1,7 +1,48 @@
+<html>
+<head>
+    <style>
+        html {
+            background-image: url("Hintergrund.jpg");
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: cover;
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .all {
+            position: absolute;
+            top: 40%;
+            left: 50%;
+            width: 300px;
+            height: 200px;
+            transform: translate(-50%, -50%);
+            -webkit-transform: translate(-50%, -50%);
+            -moz-transform: translate(-50%, -50%);
+            -o-transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+        }
+        button {
+            width: 173px;
+            border-radius: 4px;
+            background-color: lightpink;
+        }
+        button:hover {
+            background-color: lightcoral;
+        }
+
+    </style>
+
+</head>
+<body>
+
+</body>
+
+<div class="all">
 <?php
 $pdo = new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset' => 'utf8'));
-
-
 function random_string() {
     if(function_exists('random_bytes')) {
         $bytes = random_bytes(16);
@@ -18,10 +59,7 @@ function random_string() {
     }
     return $str;
 }
-
-
 $showForm = true;
-
 if(isset($_GET['send']) ) {
     if(!isset($_POST['email']) || empty($_POST['email'])) {
         $error = "<b>Bitte eine E-Mail-Adresse eintragen</b>";
@@ -29,21 +67,17 @@ if(isset($_GET['send']) ) {
         $statement = $pdo->prepare("SELECT * FROM user WHERE eMail = ?");
         $result = $statement->execute(array($_POST['email']));
         $user = $statement->fetch();
-
         if($user == false) {
             $error = "<b>Kein Benutzer gefunden</b>";
         } else {
             //Überprüfe, ob der User schon einen Passwortcode hat oder ob dieser abgelaufen ist
             $passwortcode = random_string();
             $date = date("Y-m-d H:i:s");
-
             $stmt2 = $pdo->prepare("UPDATE user SET passwortcode=:passwortcode, passwortcode_time =:passwortcode_time WHERE userID=:userID");
             $stmt2->bindParam(':passwortcode', $passwortcode);
             $stmt2->bindParam(':passwortcode_time', $date);
             $stmt2->bindParam(':userID', $user["userID"]);
             $stmt2->execute();
-
-
             $empfaenger = $user['eMail'];
             $betreff = "Neues Passwort für deinen Account auf Lima";
             $from = "From: Lima <sophia19steinhauer@gmail.com>";
@@ -51,20 +85,15 @@ if(isset($_GET['send']) ) {
             $text = 'Hallo '.$user['firstName'].',
 für deinen Account auf Lima wurde nach einem neuen Passwort gefragt. Um ein neues Passwort zu vergeben, rufe innerhalb der nächsten 24 Stunden die folgende Website auf:
 '.$url_passwortcode.'
-
 Sollte dir dein Passwort wieder eingefallen sein oder hast du dies nicht angefordert, so bitte ignoriere diese E-Mail.
-
 Viele Grüße,
 dein Lima-Team';
-
             mail($empfaenger, $betreff, $text, $from);
-
             echo "Ein Link um dein Passwort zurückzusetzen wurde an deine E-Mail-Adresse gesendet.";
             $showForm = false;
         }
     }
 }
-
 if($showForm):
     ?>
 
@@ -80,10 +109,11 @@ if($showForm):
     <form action="?send=1" method="post">
         E-Mail:<br>
         <input type="email" name="email" value=""><br>
-        <input type="submit" value="Neues Passwort">
+        <button  type="submit">Neues Passwort</button>
+        <a href="login.php">Login</a>
     </form>
-
+</div>
 <?php
 endif; //Endif von if($showForm)
 ?>
-
+</html>
