56f7a3e9795a6c0458bf6973a1be5265eeb7664d
Sinahamm <48954716+Sinahamm@users.noreply.github.com>
2019-07-14 16:48:29 +0200

    Index

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include 'notification.php';
+include 'search.php';
+include 'sidebar2.php';
+include 'files_all.php';
+?>
+<html>
+
+<head>
+    <title>Lima</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+</head>
+<body>
+<body>
+</html>
