fe67216ff26a94751ef7c6571139379846a8d801
Sophia20Steinhauer <48954723+Sophia20Steinhauer@users.noreply.github.com>
2019-07-16 11:51:46 +0200

    Add files via upload

--- answer_message.php
+++ answer_message.php
@@ -1,7 +1,49 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <title>Bootstrap</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+
+    <style>
+        html {
+            position: absolute;
+            background-image: url("Hintergrund.png");
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: cover;
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        div {
+            position: absolute;
+            top: 30%;
+            left: 35%;
+        }
+        textarea{
+
+            border: 5px;
+            border-style: solid;
+            padding: 10px;
+            width: 120%;
+            margin: 10px;
+        }
+    </style>
+
+
+<body>
+
+
 <?php
 session_start();
 $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
-
 $message = $_GET["id"];
 $statement = $pdo->prepare("SELECT * FROM message WHERE message_id = ?");
 $statement->execute(array($message));
@@ -11,14 +53,19 @@ while ($row = $statement->fetch()) {
     while ($row2 = $statement2->fetch()) {
         $empfaenger = $row2["eMail"];
     }
-?>
-<form action="writemessage_do.php" method="post">
-    <fieldset>
-        <legend>Auf Nachricht antworten</legend>
-        <label>Empfänger: <input type="text" name="receiver" value="<?php echo $empfaenger; ?>"</label>
-        <label>Betreff: <input type="text" name="subject" value="Re: <?php echo $row['message_subject']; ?>" /></label>
-        <label>Nachricht: <textarea name="content" cols="40" rows="10"></textarea></label>
-        <input type="submit" name="formaction" value="Nachricht senden" />
-    </fieldset>
-</form>
+    ?>
+    <div>
+    <form action="writemessage_do.php" method="post">
+        <fieldset>
+            <legend>Auf Nachricht antworten</legend>
+            <label>Empfänger: <input type="text" name="receiver" value="<?php echo $empfaenger; ?>"</label>
+            <label>Betreff: <input type="text" name="subject" value="Re: <?php echo $row['message_subject']; ?>" /></label> <br>
+            <label>  <textarea name="content" cols="40" rows="10"></textarea></label>
+            <br>
+            <input type="submit" name="formaction" value="Nachricht senden" />
+        </fieldset>
+    </form>
 <?php } ?>
+    </div>
+
+</body>
--- writemessage_do.php
+++ writemessage_do.php
@@ -1,64 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+
+html {
+    font-family: 'Poppins', sans-serif;
+    font-size: large;
+    position: absolute;
+    background-image: url("Hintergrund.png");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    }
+button {
+    position: center;
+    width: 173px;
+    border-radius: 4px;
+    background-color: lightpink;
+}
+button:hover {
+    background-color: lightcoral;
+}
+
+        </style>
+
 <?php
 session_start();
 $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
-
 $sender = $_SESSION['user_id'];
-
 if ($_POST['receiver'] == "") {
     $error = true;
-    echo "Bitte geben Sie einen Empfänger."."<br>";
+    echo "Bitte geben Sie einen Empfänger ein."."<br><br><a href=writemessage.php><button>Alles klar!</button></a> <a href=index.php><button>Abbrechen</button></a>";
 }
 if ($_POST['subject'] == "") {
     $error = true;
-    echo "Bitte geben Sie einen Betreff an."."<br>";
+    echo "Bitte geben Sie einen Betreff an."."<br><br><a href=writemessage.php><button>Alles klar!</button></a> <a href=index.php><button>Abbrechen</button></a>";
 }
 if ($_POST['content'] == "") {
     $error = true;
-    echo "Bitte geben Sie eine Nachricht ein"."<br>";
+    echo "Bitte geben Sie eine Nachricht ein"."<br><br><a href=writemessage.php><button>Alles klar!</button></a> <a href=index.php><button>Abbrechen</button></a>";
 }
-
-
 if(!$error) {
-
     $statement = $pdo->prepare("SELECT * FROM user WHERE eMail = ?");
     $statement->execute(array($_POST['receiver']));
     while ($row = $statement->fetch()) {
         $receiver = $row['userID'];
     }
-        if (empty($receiver)) {
-            $error = true;
-            echo "Der Empfänger existiert nicht";
-        } else  {
-            $stmt = $pdo->prepare("INSERT INTO message (message_id, sender, receiver, message_date, message_read, message_subject, content) VALUES('',:sender,:receiver, CURRENT_TIMESTAMP (), NULL , :message_subject, :content)");
-            $stmt->bindParam(':sender', $sender);
-            $stmt->bindParam(':receiver', $receiver);
-            $stmt->bindParam(':message_subject', $_POST['subject']);
-            $stmt->bindParam(':content', $_POST['content']);
-            $stmt->execute();
-
-
-            $stmt1 = $pdo->prepare("SELECT * FROM notification WHERE user_id = ?");
-            $stmt1->execute(array($receiver));
-            $result = $stmt1->rowCount();
-
-                if ($result > 0) {
-                    while ($ro = $stmt1->fetch()) {
-                        $number = $ro["number_count"];
-                        $numbernew = $number + 1;
-                        $stmt3 = $pdo->prepare("UPDATE notification SET number_count=:number_count WHERE user_id=:user_id");
-                        $stmt3->bindParam(':number_count', $numbernew);
-                        $stmt3->bindParam(':user_id', $receiver);
-                        $stmt3->execute();
-                    }
-
-                } else {
-                    $number = 1;
-                    $statement4 = $pdo->prepare("INSERT INTO notification (number_count, user_id) VALUES (?, ?)");
-                    $statement4->execute(array($number, $receiver));
-                }
-            }
-
-            echo "Nachricht wurde erfolgreich versendet!";
-
+    if (empty($receiver)) {
+        $error = true;
+        echo "Der Empfänger existiert nicht <br><br> <a href=index.php><button>Zurück zur Startseite</button></a>";
+    } else  {
+        $stmt = $pdo->prepare("INSERT INTO message (message_id, sender, receiver, message_date, message_read, message_subject, content) VALUES('',:sender,:receiver, CURRENT_TIMESTAMP (), NULL , :message_subject, :content)");
+        $stmt->bindParam(':sender', $sender);
+        $stmt->bindParam(':receiver', $receiver);
+        $stmt->bindParam(':message_subject', $_POST['subject']);
+        $stmt->bindParam(':content', $_POST['content']);
+        $stmt->execute();
+        echo "Nachricht wurde erfolgreich versendet! <br><br> <a href=index.php><button>Zurück zur Startseite</button></a>";
+    }
 }
 ?>
