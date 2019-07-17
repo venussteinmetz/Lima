68254df15111fa3e14d2e49da2842b62e5869408
Sophia20Steinhauer <48954723+Sophia20Steinhauer@users.noreply.github.com>
2019-07-17 13:42:37 +0200

    Add files via upload

--- search.php
+++ search.php
@@ -1,22 +1,50 @@
 <?php
 session_start();
 $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
-
+?>
+<?php
+include "sidebar2.php"
 ?>
     <html>
     <head>
+        <style>
+            h1 {
+                font-family: Avenir ;
+            }
+            #ergebnis {
+                position: absolute;
+                margin-top: 70px;
+                margin-right: 10px;
+                left:300px;
+                width:50%;
+            }
+            tr {
+                border-bottom: 1px solid #cbcbcb;
+                text-align: center;
+            }
+            th {
+                border-bottom: 1px solid #cbcbcb;
+                width: 10px;
+                text-align: center;
+            }
+            td {
+                width: 30px;
+                text-align: center;
+            }
+            tr:hover {
+                background: #F5F5F5;
+            }
+
+        </style>
     </head>
-    <link rel="stylesheet" href="search.php">
 
     <body>
 
-    </body>
-    </html>
+<div id="ergebnis">
 
 <?php
 $userID =$_SESSION["user_id"];
 $search =$_POST['submit-search'];
-
 if (isset($search)) {
     echo "<table id=\"files\">
         <tr>
@@ -31,20 +59,19 @@ if (isset($search)) {
             $filename = $row['filename'];
             $upload_date = $row['upload_date'];
             $mimetype = $row['mimetype'];
-
-
             echo
             " <tr>
                 <td> $filename </td>
                 <td>$upload_date</td>
                 <td>$mimetype</td>
               </tr>";
-
         }
     }
     if (empty($search)){
-        echo "Es wurde nichts eingegenben";
+        echo "<h1>Es wurde nichts eingegeben... </h1><br> <h2>Deine Dateien: </h2>";
         exit();
     }
 }
 ?>
+    </div>
+    </body>
