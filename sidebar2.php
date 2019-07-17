473e15c7270469da83dbede97fb98b12c7110c11
Sophia20Steinhauer <48954723+Sophia20Steinhauer@users.noreply.github.com>
2019-07-17 11:05:04 +0200

    Update sidebar2.php

--- sidebar2.php
+++ sidebar2.php
@@ -1,188 +1,137 @@
 <?php
 session_start();
+$pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
+
 ?>
-<doctype html>
+
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <title>Collapsible sidebar using Bootstrap 4</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
-        .wrapper {
-            display: flex;
-            width: 100%;
-            align-items: stretch;
-            position: absolute;
-        }
-        #sidebar {
-            min-width: 250px;
-            max-width: 250px;
-            min-height: 100vh;
-        }
-        #sidebar.active {
-            margin-left: -250px;
-        }
-        a[data-toggle="collapse"] {
-            position: relative;
-        }
-
-        .dropdown-toggle::after {
-            display: block;
-            position: absolute;
-            top: 50%;
-            right: 20px;
-            transform: translateY(-50%);
-            color: white;
-        }
-        @media (max-width: 768px) {
-            #sidebar {
-                margin-left: -250px;
-            }
-            #sidebar.active {
-                margin-left: 0;
-            }
-        }
         body {
+
             font-family: 'Poppins', sans-serif;
-            background: #fff;
         }
 
-        p {
-            font-family: 'Poppins', sans-serif;
-            font-size: 1.1em;
-            font-weight: 300;
-            line-height: 1.7em;
-            color: #fff;
+
+        @media screen and (max-width: 600px) {
+            .sidenav {
+                display: none;
+            }
         }
 
-        a, a:hover, a:focus {
-            color: inherit;
+        .sidenav {
+            height: 100%;
+            width: 250px;
+            position: fixed;
+            z-index: 1;
+            top: 0;
+            left: 0;
+            background-color: #F3F4F4;
+            overflow-x: hidden;
+            padding-top: 60px;
+
+        }
+
+        .sidenav a {
+            padding: 8px 8px 8px 32px;
             text-decoration: none;
-            transition: all 0.3s;
-        }
-
-        #sidebar {
-            /* don't forget to add all the previously mentioned styles here too */
-            background: lightpink;
-            color: lightpink;
-            transition: all 0.3s;
-        }
-
-        #sidebar .sidebar-header {
-            padding: 20px;
-            color: white;
-            background: lightpink;
-        }
-
-        #sidebar ul.components {
-            padding: 20px 0;
-            border-bottom: 1px solid lightpink;
-        }
-
-        #sidebar ul p {
-            color: #fff;
-            padding: 10px;
-        }
-
-        #sidebar ul li a {
-            padding: 10px;
-            font-size: 1.1em;
+            font-size: 20px;
+            color: grey;
             display: block;
-            color: white;
-        }
-        #sidebar ul li a:hover {
-            color: #fff;
-            background: lightpink;
         }
 
-        #sidebar
-        #sidebar ul li.active > a, a[aria-expanded="true"] {
-            color: #fff;
-            background: lightpink;
+        .sidenav a:hover {
+            color: black;
+            text-decoration: inherit;
         }
-        ul ul a {
-            font-size: 0.9em !important;
-            padding-left: 30px !important;
-            background: lightpink;
+
+        .hamburger-button {
+            display: block;
+            font-size:30px;
+            cursor:pointer;
+            position: absolute;
+            z-index: 10;
+        }
+
+        .profile-usertitle-name {
+            font-size: 35px;
+            padding-left: 30px;
+            align-items: center;
+            color: lightpink;
+        }
+
+        img {
+            max-width: 200px;
+            height: 50px;
+            float: right;
+            margin-right: 30px;
+            margin-bottom: 40px;
+        }
+
+        #button {
+            background-color: darkgray;
+            font-size: medium;
+            font-family: 'Poppins', sans-serif;
+            border-radius:70px;
+            display:inline-block;
+            cursor:pointer;
+            color: black;
+            padding:9px 13px;
+            text-decoration:none;
+            float: right;
+            margin-right: 40px;
+        }
+
+        #rosa_hover a:hover{
+            background-color: lightpink;
         }
 
     </style>
+</head>
+<body>
 
-    <!-- Bootstrap CSS CDN -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <!-- Our Custom CSS -->
-    <link rel="stylesheet" href="style.css">
+<div id="mySidenav" class="sidenav">
+    <a href="index.php">
+        <img src="limatransparent.png " alt="Logobild">
+    </a>
 
-    <!-- Font Awesome JS -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    <div class="profile-usertitle-name">
+        <?php
+        $statement = $pdo->prepare('SELECT * FROM user WHERE userID = ?');
+        $datensatz = array($_SESSION["user_id"]);
+        $statement->execute($datensatz);
+        if ($row = $statement->fetch()) {
+
+            echo "Hallo ", $row["firstName"]." ";
+        }
+        ?>
+    </div>
+    <div id="rosa_hover">
+
+            <a href="index.php">Meine Dateien</a>
+            <a href="#">Neuste</a>
+            <a href="favorite.php">Favoriten</a>
+            <a href="show_message.php">Meine Nachrichten</a>
+            <a href="showfolder.php">Meine Ordner</a>
+            <a href="logout.php">Logout</a>
+<br>
+    <a id="button" href="fileupload.php">Neue Datei hochladen</a>
+</div>
+</div>
+
+
+<span style="font-size:30px;cursor:pointer" class="hamburger-button">&#9776;</span>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script>
-    $(document).ready(function () {
-
-        $('#sidebarCollapse').on('click', function () {
-            $('#sidebar').toggleClass('active');
-        });
-
-    });
+    $(document).ready(function(){
+        $('.hamburger-button').click(function(){
+            $('#mySidenav').toggle();
+        })
+    })
 
 </script>
 
-
-</head>
-
-<body>
-
-
-<!-- jQuery CDN - Slim version (=without AJAX) -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<!-- Popper.JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-<!-- Bootstrap JS -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-
-
-
-<div class="wrapper">
-    <!-- Sidebar -->
-    <nav id="sidebar">
-        <div class="sidebar-header">
-            <h3>Lima</h3>
-        </div>
-
-        <ul class="list-unstyled components">
-            <p>Menü</p>
-            <li class="active">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Meine Dateien</a>
-                <ul class="collapse list-unstyled" id="homeSubmenu">
-                    <li>
-                        <a href="#">Zuletzt hinzugefügt</a>
-                    </li>
-                    <li>
-                        <a href="#">Neuste</a>
-                    </li>
-                    <li>
-                        <a href="favorite.php">Favoriten</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="show_message.php">Meine Nachrichten</a>
-            </li>
-            <li>
-                <a href="#">Geteilt mit mir</a>
-
-            </li>
-            <li>
-                <a href="#">Meine Ordner</a>
-            </li>
-
-        </ul>
-    </nav>
-
-</div>
-
 </body>
-
-
-</doctype>
+</html>
