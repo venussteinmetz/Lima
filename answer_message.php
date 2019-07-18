813f217cd714b75ee151c36a5fd7c28539d8eca4
Sinahamm <48954716+Sinahamm@users.noreply.github.com>
2019-07-18 11:12:52 +0200

    Update answer_message.php

--- answer_message.php
+++ answer_message.php
@@ -1,7 +1,12 @@
+<?php
+include "searchbar.php";
+include "sidebar2.php";
+include "notification.php";
+?>
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <title>Bootstrap</title>
+    <title>Lima</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
@@ -9,32 +14,14 @@
 
 
     <style>
-        html {
+        #answermessage  {
             position: absolute;
-            background-image: url("Hintergrund.png");
-            background-repeat: no-repeat;
-            background-position: center;
-            background-size: cover;
-            width: 100vw;
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
+            left: 300px;
+            top:80px;
+
         }
 
-        div {
-            position: absolute;
-            top: 30%;
-            left: 35%;
-        }
-        textarea{
-
-            border: 5px;
-            border-style: solid;
-            padding: 10px;
-            width: 120%;
-            margin: 10px;
-        }
+       
     </style>
 
 
@@ -48,13 +35,13 @@ $message = $_GET["id"];
 $statement = $pdo->prepare("SELECT * FROM message WHERE message_id = ?");
 $statement->execute(array($message));
 while ($row = $statement->fetch()) {
-    $statement2 = $pdo->prepare("SELECT * FROM user WHERE userID = ?");
-    $statement2->execute(array($row['sender']));
-    while ($row2 = $statement2->fetch()) {
-        $empfaenger = $row2["eMail"];
-    }
-    ?>
-    <div>
+$statement2 = $pdo->prepare("SELECT * FROM user WHERE userID = ?");
+$statement2->execute(array($row['sender']));
+while ($row2 = $statement2->fetch()) {
+    $empfaenger = $row2["eMail"];
+}
+?>
+<div id="answermessage">
     <form action="writemessage_do.php" method="post">
         <fieldset>
             <legend>Auf Nachricht antworten</legend>
@@ -65,7 +52,7 @@ while ($row = $statement->fetch()) {
             <input type="submit" name="formaction" value="Nachricht senden" />
         </fieldset>
     </form>
-<?php } ?>
-    </div>
+    <?php } ?>
+</div>
 
 </body>
