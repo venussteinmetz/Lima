96d32d2e015ff1f836bbfe02e3c56492c66d2ec1
alinabaer <47177127+alinabaer@users.noreply.github.com>
2019-07-18 14:50:30 +0200

    Alle Dateien die mit dir geteilt wurden

--- sharedfiles.php
+++ sharedfiles.php
@@ -1,9 +1,63 @@
 <?php
 session_start();
 $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
+?>
+<html>
+<head>
+    <style>
+html {
+font-family: Avenir;
+}
+.sharedwithyou {
+    position: absolute;
+    top: 170px;
+    left: 520px;
+    padding-bottom: 10px;
+}
+        #shared_files_table {
+            position: absolute;
+            margin-top: 200px;
+            margin-right: 0px;
+            left:300px;
+            width:50%;
+            height: 100px;
+            overflow-y: scroll;
+        }
+        #tr_shared_files {
+            border-bottom: 1px solid #cbcbcb;
+            text-align: center;
+        }
+        #th_shared_files {
+            width: 10px;
+            text-align: center;
+        }
+        #td_shared_files {
+            width: 10px;
+            text-align: center;
+        }
+        .downloadicon {
+            height: 25px;
+            width: 25px;
+        }
+    </style>
+</head>
+<body>
+<div class="sharedwithyou"><b>Diese Dateien wurden mit dir geteilt:</div>
 
+<table id="shared_files_table">
+    <tr id="tr_shared_files">
+        <th id="th_shared_files"> Datei </th>
+        <th id="th_shared_files"> Freigegeben von </th>
+        <th id="th_shared_files"> Download </th>
+    </tr>
+
+
+
+
+<?php
 
 $currentuser=$_SESSION["user_id"];
+
 
 $statement = $pdo->prepare("SELECT * FROM access WHERE user_id = ?");
 $statement->execute(array($currentuser));
@@ -13,22 +67,25 @@ while ($row = $statement->fetch()) {
     $statement2 = $pdo->prepare("SELECT * FROM file WHERE file_id = ?");
     $statement2->execute(array($file));
     while ($row2 = $statement2->fetch()) {
+        $filename = $row2['filename'];
+        $fileid = $row2['file_id'];
 
-        echo($row2["filename"] . "." . "$owner" . "." . $row2["filetype"]);
-            echo "<a href='https://mars.iuk.hdm-stuttgart.de/~ab247/s19_lima/download.php?filename=";
-            echo $row2['filename'].".".$row2['filetype'];
-            echo("&fileid=");
-            echo($row2['file_id']."'>");
-
-
-        echo(" Freigegeben von ");
         $sql3 = $pdo->prepare("SELECT * FROM user WHERE userID = ?");
         $sql3->execute(array($row2["owner"]));
 
         while ($row3 = $sql3->fetch()) {
-            echo($row3["eMail"]);
-        }
+            $ownerfile = $row3["eMail"];
 
+        echo "<tr>
+                    <td id='td_shared_files'>$filename</td>
+                    <td id='td_shared_files'>$ownerfile</td>
+                    <td id='td_shared_files'> <a class='link-id' href='download.php?fileid=$fileid&filename=$filename'><img class=downloadicon src='download1.png'></a></td></tr>";
+
+    }
     }
 }
             ?>
+
+</table>
+</body>
+</html>
