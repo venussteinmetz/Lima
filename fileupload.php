121d94f118fb22e3a750bb983e593ffa4af59538
Sinahamm <48954716+Sinahamm@users.noreply.github.com>
2019-07-18 16:49:35 +0200

    Update fileupload.php

--- fileupload.php
+++ fileupload.php
@@ -1,33 +1,21 @@
+<?php
+include 'searchbar.php';
+include "sidebar2.php";
+include "notification.php";
+?>
 <!DOCTYPE html>
 <html lang="de">
 <head>
     <meta charset="UTF-8">
     <title>Datei Upload</title>
     <style>
-        .all {
+        #hochladenform {
             position: absolute;
-            top: 50%;
-            left: 50%;
-            width: 100px;
-            height: 200px;
-            transform: translate(-50%, -50%);
-            -webkit-transform: translate(-50%, -50%);
-            -moz-transform: translate(-50%, -50%);
-            -o-transform: translate(-50%, -50%);
-            -ms-transform: translate(-50%, -50%);
+            left: 300px;
+            top: 80px;
+
         }
-        html {
-            background-image: url("Hintergrund.jpg");
-            background-repeat: no-repeat;
-            background-position: center;
-            background-size: cover;
-            width: 100vw;
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            font-family: 'Poppins', sans-serif;
-        }
+
         h1 {
             align-items: center;
             font-size: 30px;
@@ -44,8 +32,9 @@
     </style>
 </head>
 <body>
-<div class="all">
+<div id="hochladenform">
 <h1>Datei hochladen</h1>
+
 <form action="uploaddo.php" method="post" enctype="multipart/form-data">
     Datei auswählen:
     <input type="file" name="uploadfile" id="uploadfile"><br>
