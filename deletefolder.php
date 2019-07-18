3e3b3e0eb11327855299f82ec7ebebd97556550b
Sinahamm <48954716+Sinahamm@users.noreply.github.com>
2019-07-18 12:22:19 +0200

    Update deletefolder.php

--- deletefolder.php
+++ deletefolder.php
@@ -1,13 +1,42 @@
 <?php
-session_start();
-$pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
-
-$folderid= $_GET["folderid"];
-
-
-$statement = $pdo->prepare( "DELETE FROM folders WHERE folder_id=:folderid");
-$statement ->bindParam('folderid', $folderid);
-$statement->execute();
-echo "Ordner wurde gelöscht";
-
+include 'sidebar2.php';
+include "searchbar.php";
 ?>
+<html>
+<head>
+    <style>
+        #geloescht {
+            position: absolute;
+            left: 300px;
+            top:90px;
+        }
+        button {
+            position: relative;
+            top: 50%;
+            width: 173px;
+            border-radius: 4px;
+            background-color: lightpink;
+        }
+        button:hover {
+            background-color: lightcoral;
+        }
+
+    </style>
+</head>
+<body>
+
+
+<div id="geloescht">
+    <?php
+    session_start();
+    $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
+    $folderid= $_GET["folder_id"];
+    $statement = $pdo->prepare( "DELETE FROM folders WHERE folder_id=:folderid");
+    $statement ->bindParam('folderid', $folderid);
+    $statement->execute();
+    echo "Ordner wurde gelöscht!<br><br><a href=showfolder.php><button>Zurück zu Ordner</button></a> <a href=index.php><button>Zurück zur Startseite</button></a>";
+    ?>
+</div>
+
+</body>
+</html>
