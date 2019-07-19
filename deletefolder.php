4d6891687ad9d1bbe41b4f73c3a20853ea68aa18
Sinahamm <48954716+Sinahamm@users.noreply.github.com>
2019-07-19 11:46:23 +0200

    Update deletefolder.php

--- deletefolder.php
+++ deletefolder.php
@@ -3,6 +3,7 @@
 include 'sidebar2.php';
 include "searchbar.php";
 include "notification.php";
+
 ?>
 <html>
 <head>
@@ -12,19 +13,17 @@ include "notification.php";
             left: 300px;
             top:90px;
         }
-        #delet_button {
-            color: black;
+       #file {
             position: relative;
             top: 50%;
             width: 173px;
             border-radius: 4px;
             background-color: lightpink;
         }
-        #delet_button:hover {
-            text-decoration: none;
+        #file:hover {
             background-color: lightcoral;
-            color: black;
         }
+
     </style>
 </head>
 <body>
@@ -38,7 +37,7 @@ include "notification.php";
     $statement = $pdo->prepare( "DELETE FROM folders WHERE folder_id=:folderid");
     $statement ->bindParam('folderid', $folderid);
     $statement->execute();
-    echo "Ordner wurde gelöscht!<br><br><a href=showfolder.php><button id='delet_button'>Zurück zu Ordner</button></a> <a href=index.php><button id='delet_button'>Zurück zur Startseite</button></a>";
+    echo "Ordner wurde gelöscht!<br><br><a href=showfolder.php><button id='file'>Zurück zu Ordner</button></a> <a href=index.php><button id='file'>Zurück zur Startseite</button></a>";
     ?>
 </div>
 
