9032db33134c6081ba7ba7eb128e44e80cb7fe28
Sinahamm <48954716+Sinahamm@users.noreply.github.com>
2019-07-19 11:22:53 +0200

    Add files via upload

---
+++ delete_message.php
@@ -0,0 +1,46 @@
+<?php
+include 'sidebar2.php';
+include "searchbar.php";
+?>
+<html>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<body>
+<style>
+    .ausgabe {
+        position: absolute;
+        left: 300px;
+        top: 80px;
+        font-size: 15px;
+    }
+    #backtomessage {
+        position: relative;
+        top: 50%;
+        width: 173px;
+        border-radius: 4px;
+        background-color: lightpink;
+        color: black;
+    }
+    #backtomessage:hover {
+        background-color: lightcoral;
+    }
+
+
+</style>
+</body>
+</html>
+
+<div class="ausgabe">
+    <?php
+
+    session_start();
+    $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
+    $message = $_GET["id"];
+    $statement = $pdo->prepare("DELETE FROM message WHERE message_id = ?");
+    $statement->execute(array($message));
+    echo "Deine Nachricht wurde gelöscht!<br><br><a href=show_message.php><button id='backtomessage'>Zurück zu Nachrichten</button></a> <a href=index.php><button id='backtomessage'>Zurück zur Startseite</button></a>";
+
+    ?>
+
+</div>
+
+
