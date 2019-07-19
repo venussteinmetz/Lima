1e6125f9c5f227dfd9c2e6b85d49a730753a69a8
Sinahamm <48954716+Sinahamm@users.noreply.github.com>
2019-07-19 10:52:15 +0200

    Update writemessage.php

--- writemessage.php
+++ writemessage.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 ?>
 <?php
@@ -7,42 +6,55 @@ include 'searchbar.php';
 include "sidebar2.php";
 include "notification.php";
 ?>
-?>
+
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 <style>
-    .textfeld {
+    #textfeld {
         position: absolute;
-        top: 15%;
+        top: 80px;
         left: 300px;
-        width: 300px;
-        height: 100px;
 
     }
+    #longmail {
+        width: 400px;
+
+
+    }
+
+    button {
+        width: 173px;
+        border-radius: 4px;
+        background-color: lightpink;
+    }
+    button:hover {
+        background-color: lightcoral;
+    }
     @media screen and (min-width: 768px) and (max-width: 1024px) {
-        .textfeld {
+        #textfeld {
             width: 70%;
         }
         @media screen and (min-width: 569px) and (max-width: 767px) {
-            .textfeld{
+            #textfeld{
                 width: 70%;
             }
         }
-        </style>
+</style>
 </head>
 <body>
-<div class="textfeld">
+<div id="textfeld">
     <form action="writemessage_do.php" method="post">
         <fieldset>
             <h2>Neue Nachricht schreiben</h2>
-            <label>Empfänger: <input type="text" name="receiver" /></label>
-            <label>Betreff: <input type="text" name="subject" /></label>
-            <label>Nachricht: <textarea name="content" cols="40" rows="10"></textarea></label>
-            <input type="submit" name="formaction" value="Nachricht senden" />
+            <div id="messageschreiben">
+            <label>Empfänger: <input id="longmail" type="text" name="receiver"  /></label> <br>
+            <label>Betreff: <input id="longmail" type="text" name="subject" </label> <br>
+            <label>Nachricht: <br> <textarea name="content" cols="80" rows="20"></textarea></label> <br>
 
+                <button  type="submit">Nachricht senden</button> <br>
+            </div>
         </fieldset>
     </form>
 </div>
-
 </body>
 </html>
