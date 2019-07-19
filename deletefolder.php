cee0cc04ec6c9acd3da6f75ea3df5ae1c01eb504
Sophia20Steinhauer <48954723+Sophia20Steinhauer@users.noreply.github.com>
2019-07-19 10:40:48 +0200

    Add files via upload

--- deletefolder.php
+++ deletefolder.php
@@ -1,6 +1,8 @@
+
 <?php
 include 'sidebar2.php';
 include "searchbar.php";
+include "notification.php";
 ?>
 <html>
 <head>
@@ -10,17 +12,19 @@ include "searchbar.php";
             left: 300px;
             top:90px;
         }
-        button {
+        #delet_button {
+            color: black;
             position: relative;
             top: 50%;
             width: 173px;
             border-radius: 4px;
             background-color: lightpink;
         }
-        button:hover {
+        #delet_button:hover {
+            text-decoration: none;
             background-color: lightcoral;
+            color: black;
         }
-
     </style>
 </head>
 <body>
@@ -34,7 +38,7 @@ include "searchbar.php";
     $statement = $pdo->prepare( "DELETE FROM folders WHERE folder_id=:folderid");
     $statement ->bindParam('folderid', $folderid);
     $statement->execute();
-    echo "Ordner wurde gelöscht!<br><br><a href=showfolder.php><button>Zurück zu Ordner</button></a> <a href=index.php><button>Zurück zur Startseite</button></a>";
+    echo "Ordner wurde gelöscht!<br><br><a href=showfolder.php><button id='delet_button'>Zurück zu Ordner</button></a> <a href=index.php><button id='delet_button'>Zurück zur Startseite</button></a>";
     ?>
 </div>
 
