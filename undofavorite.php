211795053cae7a474995b56c09a020df81b08062
Sinahamm <48954716+Sinahamm@users.noreply.github.com>
2019-07-19 14:57:52 +0200

    Update undofavorite.php

--- undofavorite.php
+++ undofavorite.php
@@ -1,4 +1,35 @@
 <?php
+include 'searchbar.php';
+include "sidebar2.php";
+include "notifications.php";
+?>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        #undofav {
+            position: absolute;
+            top: 90px;
+            left:300px;
+        }
+        #fav {
+            position: relative;
+            top: 50%;
+            width: 173px;
+            border-radius: 4px;
+            background-color: lightpink;
+            color: black;
+        }
+        #fav:hover {
+            background-color: lightcoral;
+            text-decoration: none;
+        }
+
+
+    </style>
+</head>
+<div id="undofav">
+<?php
 
 session_start();
 
@@ -12,8 +43,13 @@ $statement->bindParam(':favorite', $undo );
 $statement->bindParam(':file_id', $fileid);
 if ($statement->execute()) {
 
-    echo "Die Datei ist nicht mehr unter deinen Favoriten :( ";
-    }
+    echo "Die Datei ist nicht mehr unter deinen Favoriten. <br><br><a href=favorite.php><button id='fav'>Zurück zu Favoriten</button></a> <a href=index.php><button id='fav'>Zurück zur Startseite</button></a>";;
+}
 
 
-    ?>
+?>
+</div>
+<body>
+
+</body>
+</html>
