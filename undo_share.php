3bead8ecf6862b297ad03dbf1dd34481f985a750
venussteinmetz <48954795+venussteinmetz@users.noreply.github.com>
2019-07-19 11:59:09 +0200

    Update undo_share.php

--- undo_share.php
+++ undo_share.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
 $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
-
 $usertodelete=$_GET["usertodelete"];
 $fileid=$_GET["fileid"];
-
 
 $statement2 = $pdo->prepare("SELECT access_id FROM access WHERE file_id =:file_id AND user_id=:user_id");
 $statement2->bindParam(':file_id', $fileid);
@@ -12,19 +10,15 @@ $statement2->bindParam(':user_id', $usertodelete);
 $statement2->execute();
 while ($row2 = $statement2->fetch()) {
     $accessid = $row2["access_id"];
-
     $statement = $pdo->prepare("DELETE FROM access WHERE access_id = ?");
     $statement->execute(array($accessid));
     echo "Die Freigabe wurde gelöscht";
-
     $statement3 = $pdo->prepare("SELECT * FROM access WHERE file_id = ?");
     $statement3->execute(array($fileid));
     $result = $statement3->rowCount();
-
     $statement4 = $pdo->prepare("SELECT * FROM sharing WHERE file_id = ?");
     $statement4->execute(array($fileid));
     $result2 = $statement4->rowCount();
-
     //Wenn in beiden Tabellen die File_id nichtmehr existiert, wird der Access-Status wieder auf 0 zurückgesetzt
     if ($result == 0 AND $result2 == 0) {
         $status = 0;
@@ -34,7 +28,4 @@ while ($row2 = $statement2->fetch()) {
         $stmt2->execute();
     }
 }
-
-
-
 ?>
