477b084e166dbc7ba23d1b3a3dc833d247cb6ab7
Sinahamm <48954716+Sinahamm@users.noreply.github.com>
2019-07-19 11:33:13 +0200

    Update fileupload.php

--- fileupload.php
+++ fileupload.php
@@ -13,30 +13,33 @@ include "notification.php";
             position: absolute;
             left: 300px;
             top: 80px;
+
         }
+
         h1 {
             align-items: center;
             font-size: 30px;
+
         }
-        #upload_button {
+        #file {
             width: 173px;
             border-radius: 4px;
             background-color: lightpink;
         }
-        #upload_button:hover {
+        #file:hover {
             background-color: lightcoral;
         }
     </style>
 </head>
 <body>
 <div id="hochladenform">
-    <h1>Datei hochladen</h1>
+<h1>Datei hochladen</h1>
 
-    <form action="uploaddo.php" method="post" enctype="multipart/form-data">
-        Datei auswählen:
-        <input type="file" name="uploadfile" id="uploadfile"><br>
-        <button id="upload_button" type="submit" name="action">Upload</button> <a
-    </form>
+<form action="uploaddo.php" method="post" enctype="multipart/form-data">
+    Datei auswählen:
+    <input type="file" name="uploadfile" id="uploadfile"><br>
+    <button id="file" type="submit" name="action">Upload</button> <a
+</form>
 </div>
 </body>
 </html>
