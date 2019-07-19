23459ec9b153be293c16a80107f85bcd1b4be2b4
venussteinmetz <48954795+venussteinmetz@users.noreply.github.com>
2019-07-19 11:58:48 +0200

    Update undo_externalshare.php

--- undo_externalshare.php
+++ undo_externalshare.php
@@ -1,39 +1,31 @@
 <?php
 session_start();
 $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
-
-
 $currentuser=$_SESSION["user_id"];
 $fileid=$_GET["fileid"];
 $non_user=$_GET["non_user"];
-
-
-    $statement = $pdo->prepare("SELECT * FROM files WHERE file_id= ?");
-    $statement->execute(array($fileid));
-    while ($row=$statement->fetch()) {
-        if ($row["owner"] != $currentuser){
-            exit();
-        }
+$statement = $pdo->prepare("SELECT * FROM files WHERE file_id= ?");
+$statement->execute(array($fileid));
+while ($row=$statement->fetch()) {
+    if ($row["owner"] != $currentuser){
+        exit();
     }
+}
 $statement2 = $pdo->prepare("SELECT share_id FROM sharing WHERE file =:file AND non_user=:non_user");
 $statement2->bindParam(':file', $fileid);
 $statement2->bindParam(':non_user', $non_user);
 $statement2->execute();
 while ($row2 = $statement2->fetch()) {
     $share_id = $row2["share_id"];
-
     $statement3 = $pdo->prepare("DELETE FROM sharing WHERE share_id = ?");
     $statement3->execute(array($share_id));
     echo "Die Freigabe wurde gelöscht";
-
     $statement4 = $pdo->prepare("SELECT * FROM access WHERE file_id = ?");
     $statement4->execute(array($fileid));
     $result = $statement4->rowCount();
-
     $statement5 = $pdo->prepare("SELECT * FROM sharing WHERE file_id = ?");
     $statement5->execute(array($fileid));
     $result2 = $statement5->rowCount();
-
     //Wenn in beiden Tabellen die File_id nichtmehr existiert, wird der Access-Status wieder auf 0 zurückgesetzt
     if ($result == 0 AND $result2 == 0) {
         $status = 0;
@@ -42,7 +34,5 @@ while ($row2 = $statement2->fetch()) {
         $stmt2->bindParam(':file_id', $fileid);
         $stmt2->execute();
     }
-
 }
-
 ?>
