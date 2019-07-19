48ab12ee6c0489abf6520c6c6f878da54335d21c
Sophia20Steinhauer <48954723+Sophia20Steinhauer@users.noreply.github.com>
2019-07-19 10:56:19 +0200

    Add files via upload

--- fileupload.php
+++ fileupload.php
@@ -13,33 +13,30 @@ include "notification.php";
             position: absolute;
             left: 300px;
             top: 80px;
-
         }
-
         h1 {
             align-items: center;
             font-size: 30px;
-
         }
-        button {
+        #upload_button {
             width: 173px;
             border-radius: 4px;
             background-color: lightpink;
         }
-        button:hover {
+        #upload_button:hover {
             background-color: lightcoral;
         }
     </style>
 </head>
 <body>
 <div id="hochladenform">
-<h1>Datei hochladen</h1>
+    <h1>Datei hochladen</h1>
 
-<form action="uploaddo.php" method="post" enctype="multipart/form-data">
-    Datei auswählen:
-    <input type="file" name="uploadfile" id="uploadfile"><br>
-    <button type="submit" name="action">Upload</button> <a
-</form>
+    <form action="uploaddo.php" method="post" enctype="multipart/form-data">
+        Datei auswählen:
+        <input type="file" name="uploadfile" id="uploadfile"><br>
+        <button id="upload_button" type="submit" name="action">Upload</button> <a
+    </form>
 </div>
 </body>
 </html>
