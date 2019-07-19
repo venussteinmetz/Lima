1434ff1d7a6c584c471364647b597e0b5c5f1272
alinabaer <47177127+alinabaer@users.noreply.github.com>
2019-07-19 13:04:16 +0200

    fertig gestyled

--- uploaddo.php
+++ uploaddo.php
@@ -1,8 +1,39 @@
 <?php
 session_start();
 $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
+include 'searchbar.php';
+include "sidebar2.php";
+include "notifications.php";
+include 'profilepicture.php';
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Datei Upload</title>
+    <style>
+        #upload-output {
+            position: absolute;
+            left: 350px;
+            top: 100px;
+        }
+        #upload {
+            position: relative;
+            top: 50%;
+            width: 173px;
+            border-radius: 4px;
+            background-color: lightpink;
+            color: black;
+        }
+        #upload:hover {
+            background-color: lightcoral;
+            text-decoration: none;
+        }
 
-
+    </style>
+</head>
+<body>
+<div id="upload-output">
+<?php
 
     $namearray = explode(".", $_FILES["uploadfile"]["name"]);
     $fileName = $_FILES["uploadfile"]["name"];
@@ -18,30 +49,30 @@ $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 
             $statement->execute();
             $file = $statement->fetch();
             if ($file !== false) {
-                echo 'Dieser Dateiname existiert bereits';
+                echo "Dieser Dateiname existiert bereits<br><br><a href=upload.php><button id='upload'>Zurück zum Upload</button></a><a href=index.php><button id='upload'>Zurück zur Startseite</button></a>";
                 die();
         }
 
 if (isset($namearray[2])){
-    echo ("Ungültiger Dateiname, bitte keine Punkte im Dateiname.");
+    echo "Ungültiger Dateiname, bitte keine Punkte im Dateiname<br><br><a href=upload.php><button id='upload'>Zurück zum Upload</button></a><a href=index.php><button id='upload'>Zurück zur Startseite</button></a>";
     die();
 }
 
 if($_FILES["uploadfile"]["name"]=="")
 {
-    echo "Fehler Dateiname.";
+    echo "Es wurde keine Datei ausgewählt<br><br><a href=upload.php><button id='upload'>Zurück zum Upload</button></a><a href=index.php><button id='upload'>Zurück zur Startseite</button></a>";
     die();
 }
 
 
 if ($_FILES["uploadfile"]["size"] > 25000000) {
-    echo"Datei zu groß.";
+    echo"Datei zu groß<br><br><a href=upload.php><button id='upload'>Zurück zum Upload</button></a><a href=index.php><button id='upload'>Zurück zur Startseite</button></a>";
     die();
 }
 if(!$error) {
     if ($namearray[1] == "jpg" OR $namearray[1] == "png" OR $namearray[1] == "PNG" OR $namearray[1] == "JPG" OR $namearray[1] == "jpeg" OR $namearray[1] == "gif" OR $namearray[1] == "pdf" OR $namearray[1] == "gif" OR $namearray[1] == "pdf" OR $namearray[1] == "PDF" OR $namearray[1] == "docx" OR $namearray[1] == "DOCX" OR $namearray[1] == "doc" OR $namearray[1] == "DOC" OR $namearray[1] == "php" OR $namearray[1] == "PHP" OR $namearray[1] == "html" OR $namearray[1] == "HTML" OR $namearray[1] == "css" OR $namearray[1] == "CSS" OR $namearray[1] == "xlsx" OR $namearray[1] == "XLSX" OR $namearray[1] == "xls" OR $namearray[1] == "XLS" OR $namearray[1] == "ppt" OR $namearray[1] == "PPT" OR $namearray[1] == "pptx" OR $namearray[1] == "PPTX" OR $namearray[1] == "txt" OR $namearray[1] == "TXT" OR $namearray[1] == "mp3" OR $namearray[1] == "MP3") {
     } else {
-        echo "Dateiart nicht zugelassen.";
+        echo "Dateiart nicht zugelassen<br><br><a href=upload.php><button id='upload'>Zurück zum Upload</button></a><a href=index.php><button id='upload'>Zurück zur Startseite</button></a>";
         die();
     }
 }
@@ -56,13 +87,17 @@ if(!$error) {
     $stmt->bindParam('mimetype', $mimetype);
     if ($stmt->execute()) {
         echo "Dateiname: " . $_FILES["uploadfile"]["name"] . "<br>";
-        echo "Upload erfolgreich";
+        echo "Upload erfolgreich<br><br><a href=upload.php><button id='upload'>Zurück zum Upload</button></a><a href=index.php><button id='upload'>Zurück zur Startseite</button></a>";
     }
 
 
 if (!move_uploaded_file($_FILES["uploadfile"]["tmp_name"], "/home/ab247/public_html/s19_lima/files/".$namearray[0].".".$owner.".".$namearray[1])) {
-    echo "Datei nicht hochgeladen";
+    echo "Datei nicht hochgeladen<br><br><a href=upload.php><button id='upload'>Zurück zum Upload</button></a><a href=index.php><button id='upload'>Zurück zur Startseite</button></a>";
     die();
 }
 
 ?>
+</div>
+</body>
+</html>
+
