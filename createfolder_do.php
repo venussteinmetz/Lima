609b16a9978cd962f2806c5d8b79d49f95073c6e
Sophia20Steinhauer <48954723+Sophia20Steinhauer@users.noreply.github.com>
2019-07-20 12:04:52 +0200

    Add files via upload

--- createfolder_do.php
+++ createfolder_do.php
@@ -2,36 +2,43 @@
 include "searchbar.php";
 include "sidebar2.php";
 ?>
-    <html>
-    <head>
-        <title>Lima</title>
-        <script src="http://code.jquery.com/jquery-1.7.1.js"></script>
-        <script src="js/general.js"></script>
-        <style>
-            #ausgabe {
-                font-family: 'Poppins', sans-serif;
-                font-size: medium;
-                position: absolute;
-                margin-top: 90px;
-                margin-right: 10px;
-                left:300px;
-                width:50%;
-            }
-            #file{
-                position: relative;
-                top: 50%;
-                width: 173px;
-                border-radius: 4px;
-                background-color: lightpink;
-                color: black;
-            }
-            #file:hover {
-                background-color: lightcoral;
-                text-decoration: none;
-            }
-        </style>
+<html>
+<head>
+    <title>Lima</title>
+    <script src="http://code.jquery.com/jquery-1.7.1.js"></script>
+    <script src="js/general.js"></script>
+    <style>
+        #nofolder {
+            font-family: Avenir;
+            position: absolute;
+            font-size: medium;
+            top: 90px;
+            left:300px;
+        }
+        #ausgabe {
+            font-family: Avenir;
+            font-size: medium;
+            position: absolute;
+            margin-top: 90px;
+            margin-right: 10px;
+            left:300px;
+            width:50%;
+        }
+        #file{
+            position: relative;
+            top: 50%;
+            width: 173px;
+            border-radius: 4px;
+            background-color: lightpink;
+            color: black;
+        }
+        #file:hover {
+            background-color: lightcoral;
+            text-decoration: none;
+        }
+    </style>
 
-    </head>
+</head>
 <?php
 session_start();
 $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
@@ -42,25 +49,24 @@ $statement = $pdo->prepare("SELECT * FROM folders WHERE folder_name = ? AND owne
 $statement->execute(array($foldername));
 while ($row = $statement->fetch()) {
     if ($row !== false) {
-        echo 'Dieser Ordnername existiert bereits. Bitte wähle einen anderen Namen!';
+        echo '<div id="nofolder">Dieser Ordnername existiert bereits. Bitte wähle einen anderen Namen aus!<br><br><a href=createfolder.php><button id=\'file\'>Alles klar!</button></a> <a href=index.php><button id=\'file\'>Zurück zur Startseite</button></div>';
         die();
     }
-
 }
 if ($_POST["foldername"] == "") {
-    echo "Bitte geben dem Ordner einen Namen";
+    echo '<div id="nofolder">Bitte geben Sie dem Ordner einen Namen!<br><br><a href=createfolder.php><button id=\'file\'>Alles klar!</button></a> <a href=index.php><button id=\'file\'>Zurück zur Startseite</button></div>\';
+        die();</div>';
     die();
 }
 ?>
-    <div id="ausgabe">
-<?php
-//Beim Erstellen eines neuen Ordners, wird der file_code auf 0 gesetzt. Dies bedeutet, dass noch keine Datei in dem Ordner gespeichert ist.
-$status = 0;
-$stmt = $pdo->prepare("INSERT INTO folders (folder_id, owner, folder_name, file_code) VALUES('',$owner,:foldername, :filecode)");
-$stmt ->bindParam('foldername', $foldername);
-$stmt ->bindParam('filecode', $status);
-$stmt ->execute();
-echo "Ordner wurde erstellt!<br><br><a href=showfolder.php><button id='file'>Zu meinen Ordnern</button></a> <a href=index.php><button id='file'>Zurück zur Startseite</button></a>";
-exit();
-?>
-        
+<div id="ausgabe">
+    <?php
+    //Beim Erstellen eines neuen Ordners, wird der file_code auf 0 gesetzt. Dies bedeutet, dass noch keine Datei in dem Ordner gespeichert ist.
+    $status = 0;
+    $stmt = $pdo->prepare("INSERT INTO folders (folder_id, owner, folder_name, file_code) VALUES('',$owner,:foldername, :filecode)");
+    $stmt ->bindParam('foldername', $foldername);
+    $stmt ->bindParam('filecode', $status);
+    $stmt ->execute();
+    echo "Ordner wurde erfolgreich erstellt!<br><br><a href=showfolder.php><button id='file'>Zu meinen Ordnern</button></a> <a href=index.php><button id='file'>Zurück zur Startseite</button></a>";
+    exit();
+    ?>
