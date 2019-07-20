6373554cfd475fc835f4909e71afccd0ffe6fe32
alinabaer <47177127+alinabaer@users.noreply.github.com>
2019-07-20 13:48:53 +0200

    Add files via upload

--- shared.php
+++ shared.php
@@ -13,13 +13,14 @@ $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 
             overflow-y: scroll;
             font-family: Avenir;
             position: absolute;
-            top: 270px;
+            top: 170px;
             left: 300px;
             padding-bottom: 10px;
+            min-width: 500px;
         }
         #shared_files_table {
             position: absolute;
-            margin-top: 400px;
+            margin-top: 250px;
             margin-right: 10px;
             left:300px;
             width:50%;
@@ -61,30 +62,30 @@ $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 
     </tr>
 
     <?php
-$currentuser = $_SESSION["user_id"];
-$statement = $pdo->prepare("SELECT * FROM access WHERE owner_id = $currentuser");
-$statement->execute();
+    $currentuser = $_SESSION["user_id"];
+    $statement = $pdo->prepare("SELECT * FROM access WHERE owner_id = $currentuser");
+    $statement->execute();
     while ($row = $statement->fetch()) {
         $shareduser = $row['user_id'];
         $fileid = $row['file_id'];
         $statement2 = $pdo->prepare("SELECT * FROM file WHERE file_id = $fileid");
         $statement2->execute();
-    while($row2 = $statement2->fetch()) {
-        $filename = $row2['filename'];
-        $statement3 = $pdo->prepare("SELECT * FROM user WHERE userID = $shareduser");
-        $statement3->execute();
-        while($row3 = $statement3->fetch()) {
-            $sharedmail = $row3['eMail'];
-            echo "<tr id='tr_shared_files'>
+        while($row2 = $statement2->fetch()) {
+            $filename = $row2['filename'];
+            $statement3 = $pdo->prepare("SELECT * FROM user WHERE userID = $shareduser");
+            $statement3->execute();
+            while($row3 = $statement3->fetch()) {
+                $sharedmail = $row3['eMail'];
+                echo "<tr id='tr_shared_files'>
                     <td id='td_shared_files'>$filename</td>
                     <td id='td_shared_files'>$sharedmail</td>
                     <td id='td_shared_files'><a href='undo_share.php?usertodelete=$shareduser&fileid=$fileid' ><img class=back src='rückgängig.jpg'></a></td></tr></td>
                   </tr>
                  ";
+            }
         }
     }
-}
-?>
+    ?>
 </table>
 
 </body>
