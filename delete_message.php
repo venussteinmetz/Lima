19599ae1fadbeab67e9f73b7f8cb5de143b36e61
Sophia20Steinhauer <48954723+Sophia20Steinhauer@users.noreply.github.com>
2019-07-20 13:01:08 +0200

    Add files via upload

--- delete_message.php
+++ delete_message.php
@@ -1,9 +1,11 @@
 <?php
-include 'sidebar2.php';
-include "searchbar.php";
+include "sidebar2.php";
+include "notifications.php";
+include 'searchbar.php';
+include 'profilepicture.php';
 ?>
+
 <html>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 <body>
 <style>
     .ausgabe {
@@ -23,24 +25,19 @@ include "searchbar.php";
     #backtomessage:hover {
         background-color: lightcoral;
     }
-
-
 </style>
 </body>
 </html>
 
 <div class="ausgabe">
     <?php
-
     session_start();
     $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
     $message = $_GET["id"];
     $statement = $pdo->prepare("DELETE FROM message WHERE message_id = ?");
     $statement->execute(array($message));
     echo "Deine Nachricht wurde gelöscht!<br><br><a href=show_message.php><button id='backtomessage'>Zurück zu Nachrichten</button></a> <a href=index.php><button id='backtomessage'>Zurück zur Startseite</button></a>";
-
     ?>
 
 </div>
-
 
