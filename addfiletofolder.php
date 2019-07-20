cf0c7225ed618571704b14400c4464dcb6c1a2e2
Sinahamm <48954716+Sinahamm@users.noreply.github.com>
2019-07-20 12:19:26 +0200

    Update addfiletofolder.php

--- addfiletofolder.php
+++ addfiletofolder.php
@@ -14,7 +14,7 @@ include "notification.php";
 <!DOCTYPE html>
 <html>
 <head>
-    <title></title>
+    <title>Lima</title>
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
