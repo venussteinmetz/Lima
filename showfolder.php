954915c82f60295cdaac52904b786f926b09ba5f
Sophia20Steinhauer <48954723+Sophia20Steinhauer@users.noreply.github.com>
2019-07-21 17:07:25 +0200

    Update showfolder.php

--- showfolder.php
+++ showfolder.php
@@ -1,12 +1,19 @@
 <?php
 session_start();
 $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
+if(!isset($_SESSION['user_id'])) {
+    header("location: login.php");
+    die();
+}
 include 'sidebar2.php';
 include "searchbar.php";
+include "notifications.php";
+include "profilepicture.php";
 ?>
 <!DOCTYPE html>
 <head>
     <title>Lima</title>
+    <!-- Im Style wird vor allem die Position der Tabelle und die Farbe und Position der Buttonns definiert --> 
     <style>
         html  {
             font-family: Avenir;
@@ -83,7 +90,7 @@ include "searchbar.php";
                 <th></th>
                 <th></th>
             </tr>
-
+<!-- Ordner werden aus Datenbank ausgewählt und angezeigt. Außerdem werden Buttons um Ordner zu Öffnen oder zu Löschen angezeigt --> 
             <?php
             $owner=$_SESSION["user_id"];
             $stmt = $pdo->prepare("SELECT * FROM folders WHERE owner='$owner'");
@@ -93,7 +100,7 @@ include "searchbar.php";
                 $folderid = $row["folder_id"];
                 echo "<tr>
           <td>$foldername</td>
-          
+    
           <td><a class='button-folder' href='showfilesinfolder.php?folder_name=$foldername&folder_id=$folderid'>Öffnen</a></td>
           <td><a class='button-folder' href='deletefolder.php?folder_name=$foldername&folder_id=$folderid'>Löschen</a><br></td>";
             }
