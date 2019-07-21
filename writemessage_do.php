8087e272058badc3f45f2b14750fcff58625ce8b
alinabaer <47177127+alinabaer@users.noreply.github.com>
2019-07-21 16:52:32 +0200

    Update writemessage_do.php

--- writemessage_do.php
+++ writemessage_do.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+$pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
+  
+if(!isset($_SESSION['user_id'])) {
+    header("location: login.php");
+    die();
+}
+
 include 'searchbar.php';
 include "sidebar2.php";
 include "notifications.php";
@@ -9,6 +17,7 @@ include "profilepicture.php";
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Lima</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
         #writemessage_button {
@@ -56,9 +65,9 @@ include "profilepicture.php";
 <body>
 <div id="writemessage">
     <?php
-    session_start();
-    $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
     $sender = $_SESSION['user_id'];
+    
+    //Es wird überprüft, ob alle Felder ausgefüllt wurden.
     if ($_POST['receiver'] == "") {
         $error = true;
         echo "Bitte geben Sie einen Empfänger ein.<br><br>";
@@ -73,11 +82,14 @@ include "profilepicture.php";
     };
     "<br><br> <a href=index.php><button>Zurück zur Startseite</button></a>";
     if(!$error) {
+        //Es wird überprüft ob der Nutzer auch existiert.
         $statement = $pdo->prepare("SELECT * FROM user WHERE eMail = ?");
         $statement->execute(array($_POST['receiver']));
         while ($row = $statement->fetch()) {
             $receiver = $row['userID'];
         }
+        
+        //Fehlermeldung, wenn der Nutzer nicht existiert.
         if (empty($receiver)) {
             $error = true;
             echo "Der Empfänger existiert nicht.
@@ -85,6 +97,7 @@ include "profilepicture.php";
             <br><br> <a href=writemessage.php><button id='nouser_button'>Neue Nachricht schreiben</button></a>";
             die();
         } else  {
+            //Wenn der Nutzer existiert, wird ein neuer Eintrag in die Tabelle message erstellt. 
             $stmt = $pdo->prepare("INSERT INTO message (message_id, sender, receiver, message_date, message_read, message_subject, content) VALUES('',:sender,:receiver, CURRENT_TIMESTAMP (), NULL , :message_subject, :content)");
             $stmt->bindParam(':sender', $sender);
             $stmt->bindParam(':receiver', $receiver);
@@ -92,9 +105,13 @@ include "profilepicture.php";
             $stmt->bindParam(':content', $_POST['content']);
             $stmt->execute();
             echo "Nachricht wurde erfolgreich versendet!";
+            
+            //Es wir überprüft, ob der Nutzer schon einen Eintrag in der Tabelle notification hat
             $stmt1 = $pdo->prepare("SELECT * FROM notification WHERE user_id = ?");
             $stmt1->execute(array($receiver));
             $result = $stmt1->rowCount();
+            
+            //Wenn er einen Eintrag hat, wird die aktuelle Zahl bei number_count um 1 erhöht.
             if ($result > 0) {
                 while ($ro = $stmt1->fetch()) {
                     $number = $ro["number_count"];
@@ -104,6 +121,7 @@ include "profilepicture.php";
                     $stmt3->bindParam(':user_id', $receiver);
                     $stmt3->execute();
                 }
+                //Wenn noch kein Eintrag für den Nutzer existiert, wird ein ein neuer Eintrag erstellt.
             } else {
                 $number = 1;
                 $statement4 = $pdo->prepare("INSERT INTO notification (number_count, user_id) VALUES (?, ?)");
