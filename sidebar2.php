cc4b017419f84c0d60a8c9791ac4be8309230534
Sophia20Steinhauer <48954723+Sophia20Steinhauer@users.noreply.github.com>
2019-07-21 15:07:21 +0200

    Update sidebar2.php

--- sidebar2.php
+++ sidebar2.php
@@ -1,17 +1,23 @@
 <?php
 session_start();
 $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
+if(!isset($_SESSION['user_id'])) {
+    header("location: login.php");
+    die();
+}
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Lima</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Sidebar wird zum Burgerbutton, wenn man den Bildschirm verkleinert durch @media screen. Der Username wird unter 
+dem Logoin der Sidebar angezeigt-->
     <style>
         body {
             font-family: Avenir;
         }
+
         @media screen and (max-width: 600px) {
             .sidenav {
                 display: none;
@@ -105,10 +111,11 @@ $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 
 <body>
 
 <div id="mySidenav" class="sidenav">
+    <!-- hier wird unser Logo angezeigt --> 
     <a href="index.php">
         <img src="limatransparent.png " alt="Logobild">
     </a>
-
+<!-- hier wird der Username aus der Datenbank user ermittelt und angezeigt --> 
     <div class="profile-usertitle-name">
         <?php
         $statement = $pdo->prepare('SELECT * FROM user WHERE userID = ?');
@@ -119,6 +126,7 @@ $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 
         }
         ?>
     </div>
+    <!-- hier werden die wichtigsten Seiten aufgezählt und verlinkt --> 
     <div id="rosa_hover">
 
         <a href="index.php">Meine Dateien</a>
@@ -129,16 +137,17 @@ $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 
         <a href="showfolder.php">Meine Ordner</a>
         <a href="logout.php">Logout</a>
         <br>
-
-
     </div>
+    
+    <!-- hier kann man über Buttons eine neue Datei hochladen oder freigeben --> 
     <div id="buttons">
         <a href="fileupload.php"><button id="new_file">Neue Datei hochladen</button></a>
         <a href="sharefile.php"><button id="sharefile">Datei freigeben</button></a>
     </div>
 </div>
-
-
+    
+<!-- Klickt man auf den Burgerbutton der Sidebar verschwindet diese (toogle)und man kann sie durch erneutes 
+Drücken wieder erschienen lassen --> 
 <span style="font-size:30px;cursor:pointer"  class="hamburger-button">&#9776;</span>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script>
