9cc3974bf5a4f483e09cfd32b7103bcac665e3f2
alinabaer <47177127+alinabaer@users.noreply.github.com>
2019-07-21 14:57:20 +0200

    Update download.php

--- download.php
+++ download.php
@@ -1,13 +1,20 @@
+<?php
+session_start();
+$directory = "/home/ab247/public_html/s19_lima/files"; //In diesem Directory befinden sich die gespeicherten Dateien
+$pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
+
+if(!isset($_SESSION['user_id'])) {
+    header("location: login.php");
+    die();
+}
+?>
 <!DOCTYPE html>
 <head>
     <title>Lima</title>
 </head>
 <body>
-<?php
-session_start();
-$directory = "/home/ab247/public_html/s19_lima/files";
-$pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
 
+//Die entsprechende file-id von der Datei, die heruntergeladen werden soll, wird durch die Get-Variable übergeben.
 $file=$_GET["fileid"];
 $owner = $_SESSION["user_id"];
 $statement = $pdo->prepare("SELECT * FROM file WHERE file_id = ?");
@@ -24,9 +31,10 @@ while ($row = $statement->fetch()) {
     } else {
         $filename = $_GET["filename"];
     }
+    // Der Filepath setzt sich aus dem Directory, dem Namen der Datei, einem Punkt, dem Besitzer der Datei und dem Dateityp zusammen.
     $filepath = $directory . "/" . $filename . "." . $owner . "." . $filetype;
 
-
+//Dies ist der Download-Dialog, der dazu führt, dass die Datei heruntergeladen wird. 
     header("Content-disposition: attachment; filename=$filename");
     header("Content-type: $mimetype");
     readfile($filepath);
