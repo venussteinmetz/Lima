56ab3b5c8ebf32d69f17ac4a5385779d4e07f9ec
alinabaer <47177127+alinabaer@users.noreply.github.com>
2019-07-21 14:59:58 +0200

    Update delete_file.php

--- delete_file.php
+++ delete_file.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+$directory = "/home/ab247/public_html/s19_lima/files";
+$pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
+
 include "searchbar.php";
 include "sidebar2.php";
 include "profilepicture.php";
@@ -36,9 +40,6 @@ if(!isset($_SESSION['user_id'])) {
 <body>
 <div id="delete">
 <?php
-session_start();
-$directory = "/home/ab247/public_html/s19_lima/files";
-$pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
 $fileid=$_GET["del"];
 $owner=$_SESSION["user_id"];
 $statement = $pdo->prepare("SELECT * FROM file WHERE file_id = ?");
@@ -48,15 +49,16 @@ while ($row = $statement->fetch()) {
     $filetype = $row["filetype"];
     $filename = $row["filename"];
 
-// Löschen der Datei aus der Datenbank
+// Die Datei wird aus der Datenbank gelöscht
 $statement2 = $pdo->prepare("DELETE FROM file WHERE file_id = ?");
 $statement2->execute(array($fileid));
-//Löschen der Datei von dem Server
+    
 if (empty($filename))
 {
     echo " keine Datei angegeben <br><br><a href=index.php><button id='file'>Zu meinen Datein</button></a> <a href=index.php><button id='file'>Zurück zur Startseite</button></a>";
     die();
 }
+//Die Datei wird vom Server gelöscht
 else {
     echo "Die Datei wurde gelöscht <br><br><a href=index.php><button id='file'>Zu meinen Datein</button></a> <a href=index.php><button id='file'>Zurück zur Startseite</button></a>";
     unlink($directory . "/" . $filename . "." . $owner . "." . $filetype);
