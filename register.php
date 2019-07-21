1663a2a295709649eb35ee0e763f1ba656dfb602
Sinahamm <48954716+Sinahamm@users.noreply.github.com>
2019-07-21 15:24:48 +0200

    Update register.php

--- register.php
+++ register.php
@@ -7,7 +7,9 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-    <!-- Das Dokument wird gestylt.  -->
+        <!-- Hier wird der Style definiert.
+Unter der class all wird das Formular gestylt zum Registriernen.
+Das gesamte html-tag wird gestylt mit dem Hintergrundbild. -->
     <style>
         .all {
             position: absolute;
@@ -59,6 +61,7 @@
     </style>
 </head>
 <body>
+        <!-- Formular -->
 <div class="all">
 
     <h1 id="Überschrift">Registrierung</h1>
@@ -77,7 +80,7 @@
     session_start();
     $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
     require "password_hash.php";
-
+//Überprüfung ob es Eingaben gibt
     if(isset($_GET['register'])) {
         $error = false;
         if ($_POST['firstName'] == "") {
@@ -103,6 +106,7 @@
         if (!$error) {
             require "password_hash.php";
         }
+        //Überprüfung ob email schon vergeben ist
         if (!$error) {
             $email = $_POST['eMail'];
             $statement = $pdo->prepare("SELECT * FROM user WHERE eMail = :eMail");
@@ -113,6 +117,7 @@
                 $error = true;
             }
         }
+        //Daten werden an die Datenbank übergeben 
         $statement2 = $pdo->prepare("INSERT INTO user (firstName, lastName, eMail, password) VALUES (:firstName,:lastName,:eMail,:password)");
         $statement2->bindParam(':firstName', $_POST["firstName"]);
         $statement2->bindParam(':lastName', $_POST["lastName"]);
@@ -134,7 +139,7 @@
         $statement3 = $pdo->prepare("SELECT * FROM user WHERE eMail = ?");
         $statement3->execute(array($email));
         $result = $statement3->rowCount();
-
+//Neuer Eintrag in die Datenbank für das Profilbild, indem man direkt ein Musterbild bekommt wenn man noch keins hat. 
         if ($result > 0) {
             while ($row = $statement3->fetch()) {
                 $userid = $row['userID'];
