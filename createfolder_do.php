03bc02517eb189ce12240f8166dc53ebf030a32b
Sophia20Steinhauer <48954723+Sophia20Steinhauer@users.noreply.github.com>
2019-07-21 16:15:18 +0200

    Update createfolder_do.php

--- createfolder_do.php
+++ createfolder_do.php
@@ -14,6 +14,7 @@ if(!isset($_SESSION['user_id'])) {
     <title>Lima</title>
     <script src="http://code.jquery.com/jquery-1.7.1.js"></script>
     <script src="js/general.js"></script>
+    <!-- Im Style wird die Position und Schriftart/-größe der Meldungen, sowie die Position und Farbe der Buttons definiert --> 
     <style>
         #nofolder {
             font-family: Avenir;
@@ -60,6 +61,7 @@ while ($row = $statement->fetch()) {
         die();
     }
 }
+        //Es wird überprüft, ob ein Ordnername eingegeben wurde, sonst wird eine Fehlermeldung angezeigt.
 if ($_POST["foldername"] == "") {
     echo '<div id="nofolder">Bitte geben Sie dem Ordner einen Namen!<br><br><a href=createfolder.php><button id=\'file\'>Alles klar!</button></a> <a href=index.php><button id=\'file\'>Zurück zur Startseite</button></div>\';
         die();</div>';
@@ -70,11 +72,12 @@ if ($_POST["foldername"] == "") {
     <?php
     //Beim Erstellen eines neuen Ordners, wird der file_code auf 0 gesetzt. Dies bedeutet, dass noch keine Datei in dem Ordner gespeichert ist.
     $status = 0;
+    //Der Ordner wird in die Datenbank folders eingefügt und der Nutzer bekommt eine Meldung, dass der Ordner erfolgreich erstellt wurde
     $stmt = $pdo->prepare("INSERT INTO folders (folder_id, owner, folder_name, file_code) VALUES('',$owner,:foldername, :filecode)");
     $stmt ->bindParam('foldername', $foldername);
     $stmt ->bindParam('filecode', $status);
     $stmt ->execute();
-    echo "Ordner wurde erfolgreich erstellt!<br><br><a href=showfolder.php><button id='file'>Zu meinen Ordnern</button></a> <a href=index.php><button id='file'>Zurück zur Startseite</button></a>";
+    echo "Ordner wurde erfolgreich erstellt!<br><br><a href=showfolder.php><button id='file'>Zu meinen Ordnern</button></a> <a href=index.php><button id='file'>Zurück zur Startseite</button></a>"
     exit();
     ?>
     </body>
