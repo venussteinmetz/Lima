bba34a54e90793c0181c0cdfcd39ddb8efad5e46
Sophia20Steinhauer <48954723+Sophia20Steinhauer@users.noreply.github.com>
2019-07-21 16:05:02 +0200

    Update createfolder.php

--- createfolder.php
+++ createfolder.php
@@ -2,19 +2,20 @@
 session_start();
 $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ab247', 'ab247', 'eezaS8ye3t', array('charset'=>'utf8'));
 
+if(!isset($_SESSION['user_id'])) {
+    header("location: login.php");
+    die();
+    
 include "searchbar.php";
 include "sidebar2.php";
 include "profilepicture.php";
 include "notifications.php";
-
-if(!isset($_SESSION['user_id'])) {
-    header("location: login.php");
-    die();
 }
 ?>
 <!DOCTYPE html>
 <head>
     <title>Lima</title>
+    <!-- Der Style definiert die Position des Eingabefeldes und des Buttons, sowie die Farbe des Buttons --> 
     <style>
         #createfolder {
             top:70px;
@@ -32,7 +33,8 @@ if(!isset($_SESSION['user_id'])) {
     </style>
 </head>
 <body>
-
+<!-- Ordner wird erstellt über createfolder_do.php, dazu gibt man in ein Textfeld den Namen des Ordners ein. 
+Über Button (id="file) bestätigt man den Prozess  --> 
 <form id="createfolder" action="createfolder_do.php" method="post">
     <input type="text" name="foldername" placeholder="Ordnername">
     <button id="file" type="submit">Ordner erstellen</button> 
